fcfe32d7251366b6bc7c55f745714c505c527e72
Xiaoming Zhao <apple@wireless-s0-so-150-34-46.uchicago.edu>
2016-09-19 15:44:23 -0500

    modift README

---
+++ read_ics.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+
+# --------------------------------------
+# This script is for getting information
+# from DDSM .ics files.
+# Written by Xiaoming Zhao
+# --------------------------------------
+
+import os
+import numpy as np
+import linecache 
+
+def 
