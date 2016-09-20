4b8cf953376faa05bed13c14d65ce7ecaeb72e0b
Xiaoming Zhao <Xiaoming.Zhao9@gmail.com>
2016-09-20 15:41:33 -0500

    Write script for chaning names

---
+++ change_name.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# -------------------------------------------------
+# This script is for changing names of .pnm images.
+# Written by Xiaoming Zhao
+# -------------------------------------------------
+
+import os
+import sys
+import glob
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Read DDSM\'s ics file')
+    parser.add_argument('--dir', dest='dir_path',
+                        help='Set the .pnm images\'s directory.')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    dir_path = args.dir_path
+
+    pnm_list = glob.glob(os.path.join(dir_path, '*.pnm'))
+    assert pnm_list != [],\
+        'There does not exist .ics file: {}.\n'.format(dir_path)
