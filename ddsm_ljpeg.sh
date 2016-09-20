8dc1eac3ea0b4dd2fb0a90e2d755a01bcb1da7cb
Xiaoming Zhao <apple@Xiaoming-Zhao.local>
2016-09-20 01:05:31 -0500

    Modify code for DDSM

--- ddsm_ljpeg.sh
+++ ddsm_ljpeg.sh
@@ -2,3 +2,13 @@
 
 path=/home/xmchiu/Mammo_Challenge/DDSM/test_images/test_case
 echo "path is $path"
+
+cd $path
+
+for sub_path in $(ls)
+do
+	cd $sub_path
+	cur_path=$(pwd)
+	echo "Current path is $cur_path"
+done
+
